76298b6782187748e97a0bad88b7ad7e48679940
nikivecha <jenik3751@gmail.com>
2023-11-01 19:09:12 +0300

    commit

---
+++ src/main/java/com/example/SpringBootSecond/GreetingController.java
@@ -0,0 +1,50 @@
+package com.example.SpringBootSecond;
+
+import com.example.SpringBootSecond.domain.Message;
+import com.example.SpringBootSecond.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+        return "greeting";
+    }
+
+    @GetMapping("/homeSite")
+    public String homeSite(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "homeSite";
+    }
+
+    @PostMapping("/homeSite")
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "homeSite";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
+        return "homeSite";
+    }
+}
---
+++ src/test/java/com/example/SpringBootSecond/SpringBootSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
